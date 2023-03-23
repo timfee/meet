2c29855941bda184e37718368e2e7ec9cb9ae6c0
Tim Feeley <tim@timfeeley.com>
2023-03-22 23:00:55 -0700

    Adjust for mobile viewport

--- components/Template.tsx
+++ components/Template.tsx
@@ -1,8 +1,8 @@
 export default function Template() {
   return (
     <div className="pt-8 sm:pt-16 pb-4 sm:pb-16">
-      <h1 className="text-xl font-bold tracking-tight text-accent-700 sm:text-5xl">
-        Let’s find some time
+      <h1 className="text-3xl font-bold tracking-tight text-accent-700 sm:text-5xl">
+        Let’s find some time!
       </h1>
       <p className="mt-2 sm:mt-6 sm:text-xl text-gray-800 font-medium">
         My latest availability is below. Choose a date and time that works for
