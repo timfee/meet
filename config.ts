4a49bf55b7248a034050c2525f623fa0a4f92c2f
Trillium Smith <spiteless@gmail.com>
2023-06-04 13:09:54 -0700

    asset: LEAD_TIME min 3 hours

--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ export const DEFAULT_DURATION = 60
 export const CALENDARS_TO_CHECK = ["primary", "trillium@hatsfabulous.com"]
 export const SLOT_PADDING = 0
 export const OWNER_TIMEZONE = "America/Los_Angeles"
-export const LEAD_TIME = 15
+export const LEAD_TIME = 3 * 60 // 3 hours
 
 const DEFAULT_WORKDAY = [
   {
